8ca084e120de49c09061a27f5cd5946c56386188
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2025-04-11 11:15:41 -0300

    :tada: Commit Inicial

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	
+		System.out.print("Hello World!\n");
+		System.out.print("A Turma Java 82 é TOP!");
+		
+	}
+
+}
