1b5bc6e228790f1da9fc468868c9591b44992768
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2025-04-14 10:57:55 -0300

    :sparkles: Live Code - Aula 02 - Variaveis, E/S de dados e Operadores

---
+++ aula_02/src/aula_02/Calculadora.java
@@ -0,0 +1,25 @@
+package aula_02;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+
+		System.out.println("Digite o primeiro valor: ");
+		double numero_01 = leia.nextDouble();
+		
+		System.out.println("Digite o segundo valor: ");
+		double numero_02 = leia.nextDouble();
+		
+		System.out.printf("\n%f + %f = %.2f", numero_01, numero_02, (numero_01 + numero_02));
+		System.out.printf("\n%f - %f = %.2f", numero_01, numero_02, (numero_01 - numero_02));
+		System.out.printf("\n%f * %f = %.2f", numero_01, numero_02, (numero_01 * numero_02));
+		System.out.printf("\n%f / %f = %.2f", numero_01, numero_02, (numero_01 / numero_02));
+		System.out.printf("\n%f ^ %f = %.2f", numero_01, numero_02, Math.pow(numero_01, numero_02));
+		System.out.printf("\n%f R %f = %.2f", numero_01, numero_02, Math.sqrt(numero_01));
+	}
+
+}
